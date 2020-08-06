98df48e78c4494e30815f5695ca53342f36d75f5
newman15 <47096479+newman15@users.noreply.github.com>
2020-08-05 22:09:57 -0700

    Admin Loggin Functionality

--- dbPool.js
+++ dbPool.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: "e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "ulntjmg4s0ls8gih",
+//     password: "i2s2r0s41hkgixiq",
+//     database: "rz4jr3j5uk6xfi7e"
+// });
+
+var pool = mysql.createPool({
     connectionLimit: 10,
-    host: "e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "ulntjmg4s0ls8gih",
-    password: "i2s2r0s41hkgixiq",
-    database: "rz4jr3j5uk6xfi7e"
+    host: "localhost",
+    user: "root",
+    password: "A@n3w1515",
+    database: "authentication"
 });
 
 module.exports = pool;
