acd176824d765325802d197378b18758da90bf1b
rtalmage <raymondtalmage@gmail.com>
2020-08-13 23:29:09 -0700

    db

--- dbPool.js
+++ dbPool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool  = mysql.createPool({
     connectionLimit: 10,
-    host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "v7pczzk0eqtrq7zl",
-    password: "sa2fs8g9gxfdpvnx",
-    database: "ofnnwyo6iqci6okx"
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "finalproj"
 });
 
 module.exports = pool;
