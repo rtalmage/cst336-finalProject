0c4b3654de09c54c3d6da4b2f6e8514718693e52
rtalmage <raymondtalmage@gmail.com>
2020-08-14 02:57:26 -0700

    updated db creds

--- dbPool.js
+++ dbPool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "starwars1",
-    database: "final_project"
+    host: "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "e4n3qqumdxsiey5f",
+    password: "judeq9ub5ktakpju",
+    database: "jk1gq92mkz0tngpk"
 });
 
 module.exports = pool;
