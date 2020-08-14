834d661e756189bf5d226f79dbde506920cab39a
newman15 <47096479+newman15@users.noreply.github.com>
2020-08-14 02:48:24 -0700

    Final Merge

--- dbPool.js
+++ dbPool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "A@n3w1515",
-    database: "final_project"
+    host: "wvulqmhjj9tbtc1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "abaz6kn5dcgh4pmj",
+    password: "j3v861cd1y3dy05h",
+    database: "be5hnzvy1459i4mr"
 });
 
 module.exports = pool;
